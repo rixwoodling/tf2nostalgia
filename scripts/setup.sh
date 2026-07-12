4de71b4a76b83a3f670363bec3f760b0e44a15c7
Rix Woodling <rixwoodling@gmail.com>
2026-07-12 15:30:05 -0700

    Add dependency installation to setup

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -10,29 +10,35 @@ check_platform()
         echo "ERROR: Linux is required."
         exit 1
     }
-
     [[ "$(uname -m)" == "x86_64" ]] || {
         echo "ERROR: Linux x86_64 is required."
         exit 1
     }
-
     source /etc/os-release
-
     echo "Detected: $PRETTY_NAME"
-
     if [[ "$ID" != "ubuntu" ]]; then
         echo "WARNING: This installer is developed and tested on Ubuntu."
     fi
-
     echo
 }
 
 install_dependencies()
 {
+    echo "Installing dependencies..."
+    sudo dpkg --add-architecture i386
+    sudo apt update
+    sudo apt install -y \
+        curl \
+        git \
+        tar \
+        lib32gcc-s1 \
+        lib32stdc++6
+    echo
 }
 
 install_steamcmd()
 {
+    exit 1
 }
 
 install_tf2()
